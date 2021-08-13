496a182870594672b6b4fa7acfa23534e76b1cf1
idinyte <black.mad.eagle@gmail.com>
2021-08-13 21:13:13 +0300

    keypad support

--- script.js
+++ script.js
@@ -1,63 +1,93 @@
 let result = document.querySelector("#results h1");
 let a = [];
-let b = []; 
+let b =[];
 let operator;
+let operations = {"add": (a,b)=>a+b, "substract": (a,b)=>a-b, "multiply": (a,b)=>a*b, "divide": (a,b)=>a/b};
 let numbers = {"zero":0,"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7,"eight":8, "nine":9};
-let operations = {"add": (a,b)=>a+b, "substract": (a,b)=>a-b, "multiply": (a,b)=>a*b, "divide": (a,b)=>a/b}
+let keycodes ={96:"zero",97:"one", 98:"two", 99:"three", 100:"four", 101:"five", 102:"six", 103:"seven",104:"eight", 105:"nine", 107:"add", 109:"substract", 106:"multiply", 111:"divide", 110:"dot", 46:"clear", 8:"backspace", 13:"equal"}
 
 let buttons = document.querySelectorAll(".buttons");
 buttons.forEach(button => button.addEventListener("click", operate));
 
-function operate(){
-	if(this.id in numbers){
-		if(operator == null){
-			a.push(numbers[this.id]);
-			result.textContent = a.join().replaceAll(",","");
-			console.log(typeof operator);
-		}
-		else{
-			b.push(numbers[this.id]);
-			result.textContent = b.join().replaceAll(",","");
-			console.log(typeof a);
-		}
+//keypad support
+document.addEventListener("keydown", function(e){operate(e, keycodes[e.keyCode])});
+//button highlight on keypad press
+document.addEventListener("keydown", function(e){highlight(e, keycodes[e.keyCode], true)});
+document.addEventListener("keyup", function(e){highlight(e, keycodes[e.keyCode], false)});
+
+function operate(e, id = this.id){
+	if(id in numbers){
+		currentArray().push(numbers[id]);
+		result.textContent = currentArray().join().replaceAll(",","");
 	}
-	else if(this.id in operations && (a.length > 0 || typeof a === "number")){
-		 operator = operations[this.id];
+	else if(id in operations && (a.length > 0 || typeof a === "number")){
+		 operator = operations[id];
+		 console.log("poop");
 	}
-	else if(this.id == "change-sign"){
+	else if(id == "change-sign"){
 		if(result.textContent.indexOf("-") == 0)
 		{
 			result.textContent = result.textContent.slice(1);
 			if(!Array.isArray(a)) a *= -1;
-			else operator == null || !Array.isArray(a) ? a.shift("-"): b.shift("-");
+			else currentArray().shift("-");
 		}
 		else 
 		{ 
 			result.textContent = "-"+result.textContent;
 			if(!Array.isArray(a)) a *= -1;
-			else operator == null ? a.unshift("-"): b.unshift("-");
+			else currentArray().unshift("-");
 		}
 	}
-	else if(this.id == "percentage" && parseFloat(result.textContent) != 0){
-		result.textContent = (parseFloat(result.textContent)/100).toString();
-		if(!Array.isArray(a)) a /= 100;
-		else operator == null ? (parseFloat(a.join().replaceAll(",",""))/100).split(""): (parseFloat(b.join().replaceAll(",",""))/100).split("");
+	else if(id == "percentage" && parseFloat(result.textContent) != 0){
+		if(operator==null){
+			result.textContent = (Math.round(10**9*(parseFloat(result.textContent)))/10**11).toString();
+			!Array.isArray(a) ? a /= 100 : a = result.textContent.split("");
+		}
+		else{
+			operate(null, "equal");
+			a /= 100;
+			result.textContent = a.toString();
+		}
 	}
-	else if(this.id == "dot" && result.textContent.indexOf(".") == -1){
+	else if(id == "dot" && result.textContent.indexOf(".") == -1){
 		result.textContent = result.textContent + ".";
-		operator == null ? a.push("."): b.push(".");
+		currentArray().push(".");
 	}
-	else if(this.id == "equal" && b.length > 0){
-		Array.isArray(a) ? result.textContent = Math.round(operator(parseFloat(a.join().replaceAll(",","")), parseFloat(b.join().replaceAll(",","")))*100000)/100000 : result.textContent = Math.round(operator(a, parseFloat(b.join().replaceAll(",","")))*100000)/100000;
+	else if(id == "equal" && b.length > 0){
+		if(parseFloat(b.join().replaceAll(",","")) == 0 && operator == operations.divide){
+			result.textContent = "Math ERROR";
+			a = []
+			b = [];
+			operator = null;
+		}
+		console.log(a);
+		console.log(b);
+		Array.isArray(a) ? result.textContent = Math.round(operator(parseFloat(a.join().replaceAll(",","")), parseFloat(b.join().replaceAll(",","")))*10**11)/10**11 : Math.round(operator(a, parseFloat(b.join().replaceAll(",","")))*10**11)/10**11;
 		a = parseFloat(result.textContent);
 		b = [];
+		operator = null;
 	}
-	else if(this.id == "clear"){
+	else if(id == "clear"){
 		result.textContent = "0";
 		a = []
 		b = [];
 		operator = null;
 	}
+	else if(id == "backspace"){
+		currentArray().pop();
+		result.textContent = currentArray().join().replaceAll(",","");
+	}
+}
 
-	operator == null ? console.log(a): console.log(b);
+function currentArray(){
+	return operator == null && Array.isArray(a) ?  a: b;
+}
+
+function highlight(e, id, turnOn){
+	if(id != "equal"){
+		turnOn ? document.getElementById(id).classList.add("grey-highlight"): document.getElementById(id).classList.remove("grey-highlight");
+	}
+	else{
+		turnOn ? document.getElementById(id).classList.add("green-highlight"): document.getElementById(id).classList.remove("green-highlight");
+	}
 }
